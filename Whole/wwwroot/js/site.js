fd768195aadf4b585346d87c685ee7a57e1f0ba2
Rama Subramanian T <109201625+RamaSubramanianT@users.noreply.github.com>
2024-02-23 17:33:56 +0530

    Update

---
+++ Whole/wwwroot/js/site.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+    $('#deleteButton').click(function (e) {
+        e.preventDefault();
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $('form').unbind('submit').submit();
+                Swal.fire({
+                    title: "Deleted!",
+                    text: "Your file has been deleted.",
+                    icon: "success"
+                });
+            }
+        });
+    });
+});
